8a25da419311d7ced9a1dd6a9ac917faafc49a7f
islam walied <islam.walied96@gmail.com>
2024-02-28 20:50:30 +0200

    adding login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $remember = !empty($request->remember);
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password],$remember))
+        {
+            return redirect('dashboard');
+        }
+        else
+            return redirect()->back()->message('error','Please Enter Correct Email and Password');
+    }
+    public function logout()
+    {
+        Auth::logout();
+        return view('auth.login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[\App\Http\Controllers\AuthController::class,'index']);
+Route::post('/login',[\App\Http\Controllers\AuthController::class,'login'])->name('login');
+Route::get('/logout',[\App\Http\Controllers\AuthController::class,'logout'])->name('logout');
 
 Route::get('/dashboard', function () {
     return view('dashboard.index');
